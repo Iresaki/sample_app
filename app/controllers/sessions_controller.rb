fc27fbd6485827f42bd3ce259372a163d08adcc2
Dustin Eis <Iresaki@gmail.com>
2013-05-01 23:07:15 -0700

    Finish user edit, update, index, and destroy actions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,9 +7,9 @@ class SessionsController < ApplicationController
 		user = User.find_by_email(params[:session][:email].downcase)
 		if user && user.authenticate(params[:session][:password])
 			sign_in user
-			redirect_to user
+			redirect_back_or user
 		else
-			flash.now[:error] = 'Invalid email/password combination' # Not quite right!
+			flash.now[:error] = 'Invalid email/password combination' # flash.now works with render, flash does not
 			render 'new'
 		end
 	end
@@ -18,4 +18,12 @@ class SessionsController < ApplicationController
 		sign_out
 		redirect_to root_url
 	end
+
+	private
+
+		def redirect_signed_in_user
+	      if signed_in?
+	      	redirect_to root_path, notice: "You are already signed in."
+	      end
+	    end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,14 +1,23 @@
 class UsersController < ApplicationController
+  before_filter :signed_in_user, only: [:index, :edit, :update, :destroy]
+  before_filter :correct_user,   only: [:edit, :update]
+  before_filter :admin_user,     only: :destroy
+
+  def index
+    @users = User.paginate(page: params[:page])
+  end
 
   def show
   	@user = User.find(params[:id])
   end
   
   def new
+    redirect_signed_in_user
   	@user = User.new
   end
 
   def create
+    redirect_signed_in_user
   	@user = User.new(params[:user])
   	if @user.save
       sign_in @user
@@ -18,4 +27,51 @@ class UsersController < ApplicationController
   		render 'new'
   	end
   end
+
+  def edit
+  end
+
+  def update
+    if @user.update_attributes(params[:user])
+      flash[:success] = "Profile updated"
+      sign_in @user
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    if !User.find(params[:id]).admin?
+      User.find(params[:id]).destroy
+      flash[:success] = "User destroyed."
+      redirect_to users_url
+    else
+      redirect_to root_path
+    end
+  end
+
+  private
+
+    def signed_in_user
+      unless signed_in?
+        store_location
+        redirect_to signin_url, notice: "Please sign in."
+      end
+    end
+
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_path) unless current_user?(@user)
+    end
+
+    def admin_user
+      redirect_to(root_path) unless current_user.admin?
+    end
+
+    def redirect_signed_in_user
+      if signed_in?
+        redirect_to root_path, notice: "You are already signed in."
+      end
+    end
 end
